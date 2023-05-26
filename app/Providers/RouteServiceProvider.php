eda04cb8b1ff83482e0f7a646d90067600165210
manh <manh.nguyen@alobridge.com>
2023-05-26 20:07:32 +0700

    optimize: update directory structure

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -34,7 +34,9 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
-                ->group(base_path('routes/web.php'));
+                ->group(base_path('routes/admin.php'));
+            Route::middleware('web')
+                ->group(base_path('routes/end-user.php'));
         });
     }
 }
