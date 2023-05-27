4ade33b1c620ae4010d56c7c9f3ef93cf4ad5d10
manh <manh.nguyen@alobridge.com>
2023-05-27 21:35:12 +0700

    feature: add vite to project

---
+++ resources/views/end-user/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.main')
+
+@section('title')
+    Hello Bong Beo
+@endsection
+
+@section('content')
+    <h1>Hello Bong Be1o</h1>
+@endsection
--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -5,11 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>@yield('title')</title>
+
+    @vite('resources/assets/scss/app.scss', 'resources/js/app.js')
 </head>
 
 <body>
-
+    @yield('content')
 </body>
 
 </html>
